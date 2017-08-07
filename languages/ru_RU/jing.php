c7347c083d8e11d31743101ebbbd92355ddfdeab
yulipu <ylp1127@126.com>
2017-08-07 22:08:53 +0800

    change lang

--- languages/ru_RU/jing.php
+++ languages/ru_RU/jing.php
@@ -1,15 +1,15 @@
 <?php
 return array(
-    'jinghao' => '№',
-    'chejian' => 'Семинар',
-    'chulichang' => '处理厂',
-    'qukuai' => '区块',
-    'kaifadanyuan' => '开发单元',
-    'cengwei' => '层位',
-    'caiyouchang' => 'нефтяной завод',
-    'muqianjingbie' => '目前井别',
-    'touchanriqi' => 'время запуска',
-    'jiliangjian' => '计量间',
-    'youqitian' => '油气田',
-    'chakan' => '查看详情'
+    'jinghao' => '№ скв.',
+    'chejian' => 'цех',
+    'chulichang' => 'ГПЗ',
+    'qukuai' => 'блок',
+    'kaifadanyuan' => 'элемент по разработке',
+    'cengwei' => 'пачка',
+    'caiyouchang' => 'НГДУ',
+    'muqianjingbie' => 'текущий тип скв.',
+    'touchanriqi' => 'дата ввода в эксплуатацию',
+    'jiliangjian' => 'АГЗУ',
+    'youqitian' => 'месторождение',
+    'chakan' => 'Посматреть подробнее'
 );
